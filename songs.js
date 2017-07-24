9ccd6c18a4190f5b53617eb529a93f65a334cc18
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-24 14:02:12 -0500

    Styles closer on add view

--- songs.js
+++ songs.js
@@ -11,13 +11,9 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 songs[songs.length] = "Love's In Need Of Love Today - by Stevie Wonder on the album Songs In The Key Of Life";
 
-// console.log(songs)
 songs.toString();
-// console.log(songs)
-for (let i = 0; i < songs.length; i++){
-    // console.log(songs[i]);
-    let song_string = songs[i].replace('*' , '').replace('@' , '').replace('(', '').replace('!', '').replace('>','-');
-    // console.log(song_string);
+songs.forEach((song)=>{
+    let song_string = song.replace('*' , '').replace('@' , '').replace('(', '').replace('!', '').replace('>','-');
 
     // *****SONG TITLE*******
     let song_title = song_string.split('-')
@@ -32,33 +28,26 @@ for (let i = 0; i < songs.length; i++){
 
 
     // *****ALBUM NAME******
-
     let album_name = song_string.split('album')
-
     album_name = album_name[1]
 
-    console.log(album_name)
 
 
+    // *****JS RENDERING******
 
-    let song_string_edit = `<div class="title-container"<h4>${song_title}</h4></div>
-                             <div class="description container"><p>${artist_name} |${album_name} | Genre</p><p></p></div>`
+    let song_string_edit = `<div class="song-container"><title>${song_title}</title><p>${artist_name} |${album_name} | Genre</p></div>`
 
     let placeholder = document.querySelector('div.song-list');
     placeholder.innerHTML += song_string_edit;
-    // console.log('placeholder', placeholder)
-    // console.log('song string', song_string)
-}
 
+});
 
+// **********VIEW HIDE SHOW**********
 let add_form = document.querySelector('form');
 let add_button = document.querySelector('#add')
-// console.log(add_form)
-
-
 add_button.addEventListener('click', ()=>{
     add_form.classList.toggle('hide')
-})
+});
 
 
 
