1d8e4c4ece231839c37cc1c67bb1153218f3f023
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-24 15:53:15 -0500

    Excercise complete, with add view working.  Added a jQuery slider that isnt working

--- songs.js
+++ songs.js
@@ -10,10 +10,45 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 songs[songs.length] = "Love's In Need Of Love Today - by Stevie Wonder on the album Songs In The Key Of Life";
-
 songs.toString();
 songs.forEach((song)=>{
-    let song_string = song.replace('*' , '').replace('@' , '').replace('(', '').replace('!', '').replace('>','-');
+    var song_string = song.replace('*' , '').replace('@' , '').replace('(', '').replace('!', '').replace('>','-');
+    song_parser(song_string)
+});
+
+// **********VIEW HIDE SHOW**********
+let add_form = document.querySelector('form');
+let add_button = document.querySelector('#add')
+add_button.addEventListener('click', ()=>{
+    add_form.classList.toggle('hide')
+});
+
+// ************ADD VIEW FUNCTIONALITY*****************
+
+let artist_input = document.querySelector('#Artist');
+let album_input = document.querySelector('#Album');
+let song_input = document.querySelector('#Song')
+let submit_button = document.querySelector('#lets-hear-it-button')
+
+submit_button.addEventListener('click', input_handler());
+
+
+function input_handler() {
+    let new_song = (song_input.value + ' - by ' + artist_input.value + ' on the album ' + album_input.value)
+    songs.push(new_song)
+    song_parser(new_song)
+
+
+}
+
+submit_button.addEventListener('click', ()=>{
+    input_handler(song_input.value, artist_input.value, album_input.value)
+
+});
+
+
+function song_parser(song_string) {
+
 
     // *****SONG TITLE*******
     let song_title = song_string.split('-')
@@ -24,7 +59,6 @@ songs.forEach((song)=>{
     artist_name = artist_name.shift()
     by_index = artist_name.indexOf('by')
     artist_name = artist_name.slice(by_index + 3,)
-    console.log(artist_name)
 
 
     // *****ALBUM NAME******
@@ -39,15 +73,12 @@ songs.forEach((song)=>{
 
     let placeholder = document.querySelector('div.song-list');
     placeholder.innerHTML += song_string_edit;
+}
 
-});
 
-// **********VIEW HIDE SHOW**********
-let add_form = document.querySelector('form');
-let add_button = document.querySelector('#add')
-add_button.addEventListener('click', ()=>{
-    add_form.classList.toggle('hide')
-});
+
+
+
 
 
 
