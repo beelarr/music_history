7b36c703b9afda20f60206c2c4a6aade04abe0b6
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-29 21:03:27 -0500

    Added the json, pagination, delete buttons, and styles.

--- Javascripts/songs.js
+++ Javascripts/songs.js
@@ -30,21 +30,94 @@ let album_input = document.querySelector('#Album');
 let song_input = document.querySelector('#Song')
 let submit_button = document.querySelector('#lets-hear-it-button')
 
-submit_button.addEventListener('click', input_handler());
+// submit_button.addEventListener('click', input_handler());
 
 
-function input_handler() {
-    let new_song = (song_input.value + ' - by ' + artist_input.value + ' on the album ' + album_input.value)
-    songs.push(new_song)
-    song_parser(new_song)
-
-
-}
+// function input_handler() {
+//     let new_song = (song_input.value + ' - by ' + artist_input.value + ' on the album ' + album_input.value)
+//     songs.push(new_song)
+//     song_parser(new_song)
+//
+//
+// }
 
 submit_button.addEventListener('click', ()=>{
     input_handler(song_input.value, artist_input.value, album_input.value)
 
 });
+
+
+
+
+// **************PAGINATION OF FORM***********************
+
+
+$(document).ready(function () {
+
+
+    page_size = 10;
+    page_count = $('.one-row').length;
+    var current_page = 1
+
+
+    // ********PREPARE NAV**********
+
+    var nav = ''
+    var total_pages = Math.ceil(page_count / page_size)
+    for (var i = 0; i < total_pages; i++){
+        // nav += `<li class='number'><a href="#"> ${i + 1} </a></li> `
+    }
+    $('.page_prev').after(nav);
+    $('.number').first().addClass('active')
+
+        // *************************
+
+show_page = function () {
+        $('.one-row').hide().each(function (n) {
+            if (n >= page_size * (current_page - 1) && n < page_size * current_page)
+                $(this).show();
+
+        });
+
+}
+show_page();
+
+    $(".pagination li.number").click(function () {
+        $(".pagination li").removeClass('active');
+        $(this).addClass('active');
+        current_page = parseInt($(this).text());
+        show_page()
+
+    });
+    $('.pagination li.page_prev').click(function () {
+        if ($(this).next().is('active')) return;
+    $('.number.acitve').removeClass('active').prev().addClass('active');
+    current_page = current_page > 1 ? (current_page - 1) : 1;
+    show_page()
+
+    });
+
+    $('.pagination li.page-next').click(function () {
+        if ($(this).prev().is('active')) return;
+
+    $('.number.active').removeClass('active').next().addClass('active');
+
+        current_page = current_page < total_pages ? (current_page + 1) : total_pages;
+        show_page()
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
 
 
 // function song_parser(song_string) {
--- Javascripts/xhr.js
+++ Javascripts/xhr.js
@@ -1,3 +1,58 @@
 /**
  * Created by beelarr on 7/29/17.
  */
+{
+    var music_log = {};
+    let music = [];
+
+
+    music_log.load_items = function () {
+        var loader = new XMLHttpRequest()
+        loader.addEventListener('load', function () {
+            music = JSON.parse(this.responseText);
+            loader_of_music(music);
+        });
+
+        loader.open('GET', 'music.json')
+        loader.send()
+    }
+
+}
+
+{
+    function loader_of_music(music) {
+        music.forEach((e)=>{
+            document.querySelector('.data').innerHTML +=
+            `<tr class="one-row">
+                  <th class="number" scope="row">${e.Number}</th>
+                  <td class="year" >${e.Year}</td>
+                  <td class="album">${e.Album}</td>
+                  <td class="artist">${e.Artist}</td>
+                  <td class="genre">${e.Genre}</td>
+                  <td>
+                      <p  data-placement="top" data-toggle="tooltip" title="Delete">
+                        <button id="delete-btn" class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >
+                          <span class="glyphicon glyphicon-trash"></span>
+                        </button>
+                        </p>
+                  </td>
+              </tr>`
+        });
+
+
+
+    }
+}
+music_log.load_items(loader_of_music)
+
+
+// *******************DELETE BUTTON************************
+
+
+
+$('.one-row').on('click', $('#delete-btn'), function () {
+    $(this).closest('tr').remove()
+    console.log('jquery')
+})
+
+
